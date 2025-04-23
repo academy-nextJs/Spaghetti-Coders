f9a774787167362fe9c9b086d116dfe968bdd1e0
Pouya <titanpooyanote@gmail.com>
2025-04-23 16:20:14 +0330

    SC_2: initial config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.API_URL,
+  },
   /* config options here */
 };
 
