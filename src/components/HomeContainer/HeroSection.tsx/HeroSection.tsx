3cfcd212c60514aa28ae65df90c2bd543a2e9e05
qhoncheh <ghoncheh.ataee@gmail.com>
2025-04-24 11:39:43 +0330

    ✔️

--- src/components/HomeContainer/HeroSection.tsx/HeroSection.tsx
+++ src/components/HomeContainer/HeroSection.tsx/HeroSection.tsx
@@ -6,7 +6,6 @@ const HeroSection = () => {
     return (
         <div className="container h-1/4 mx-auto relative w-full max-w-[1200px] bg-white overflow-hidden ">
             <div className="flex flex-col md:flex-row items-center gap-8">
-                {/* Left side content */}
                 <div className="w-full md:w-1/2 px-6 py-12 md:py-20 md:pl-12 lg:pl-20 z-10">
                     <div className="max-w-2xl mt-12">
                         <h4 className="text-right text-xl  font-bold text-gray-800 mb-2">
@@ -42,17 +41,15 @@ const HeroSection = () => {
                     </div>
                 </div>
 
-                {/* Right side image */}
                 <div className="w-full md:w-1/3 flex  h-96 rounded-2xl relative">
                     <Image
-                        src="/hero.png" // Replace with your actual image path
+                        src="/hero.png" 
                         alt="Modern apartment building"
                         layout="fill"
                         objectFit="cover"
                         className="rounded-3xl"
                     />
 
-                    {/* Filter button */}
                     <div className="absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2">
                         <FilterButton />
                     </div>
