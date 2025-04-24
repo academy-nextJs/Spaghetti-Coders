7ca917442ef22da9bfbfe786a0664614582e558c
qhoncheh <ghoncheh.ataee@gmail.com>
2025-04-24 11:34:37 +0330

    ✔️

---
+++ src/components/HomeContainer/page.tsx
@@ -0,0 +1,12 @@
+
+import HeroSection from './HeroSection.tsx/HeroSection';
+
+export default function Home() {
+    return (
+        <div>
+            <main>
+                <HeroSection />
+            </main>
+        </div>
+    );
+}
