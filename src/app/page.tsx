3d6a0856c1dc693b43b0b41f2389461c310739e5
Shayan <nwryshayan500@gmail.com>
2025-04-26 19:38:10 +0330

    Task Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import LandingCard1 from "../components/common/landing-card-1";
-import LandingCard2 from "../components/common/landing-card-2";
 
 export default function Home() {
   return (
     <>
-      <div className="grid grid-cols-4">
-        <LandingCard1 />
-      </div>
-
-      <div className="grid grid-cols-4">
-        <LandingCard2 title="آپارتمان لوکس زعفرانیه" location="تهران، زعفرانیه" bathroom={2} bedroom={2} parking={2} yard price={150000} originalPrice={200000} rating={2} discountPercentage={15} />
-      </div>
+      <div>Landing Page</div>
     </>
   );
 }
--- src/components/common/LandingHouseCard.tsx
+++ src/components/common/LandingHouseCard.tsx
@@ -7,31 +7,49 @@ import {
 import Image from "next/image";
 import Pic from '@/public/Mansion.png'
 import { HouseCardProps } from "@/src/types/types";
-import { BathroomIcon, BedroomIcon, MapLocationIcon, ParkingIcon, RatingHeartIcon, TomanIcon, YardIcon } from "@/src/assets/sgvs";
+import { BathroomIcon, BedroomIcon, MapLocationIcon, ParkingIcon, RatingHeartIcon, TomanIcon, YardIcon } from "@/src/assets/SGVs";
+import Carousel from "./carousel";
 
-export default function LandingCard2({ title, location, bathroom, bedroom, parking, yard, price, originalPrice, rating, discountPercentage }: HouseCardProps) {
+export default function LandingHouseCard({ title, location, bathroom, bedroom, parking, yard, price, originalPrice, rating, discountPercentage }: HouseCardProps) {
   return (
-    <div className="relative w-[300px] rounded-3xl overflow-hidden">
-      <Image
-        alt="Luxury apartment view"
-        className="object-cover w-full h-1/2 rounded-3xl"
-        src={Pic.src}
-        width={300}
-        height={280}
-      />
+    <div className="relative w-[300px] overflow-hidden">
+      <Carousel slidesPerView={1} houseCarousel>
+        <Image
+          alt="Luxury apartment view"
+          className="object-cover w-full aspect-square rounded-3xl"
+          src={Pic.src}
+          width={300}
+          height={280}
+        />
+        <Image
+          alt="Luxury apartment view"
+          className="object-cover w-full aspect-square rounded-3xl"
+          src={Pic.src}
+          width={300}
+          height={280}
+        />
+        <Image
+          alt="Luxury apartment view"
+          className="object-cover w-full aspect-square rounded-3xl"
+          src={Pic.src}
+          width={300}
+          height={280}
+        />
+      </Carousel>
+
       <div className="absolute top-2 left-2 z-10 flex items-center gap-1 text-sm font-medium text-white">
         {discountPercentage ?
           <div className="bg-[#FF5555] flex items-center rounded-full h-full p-1.5">
             <span className="px-1">%{discountPercentage}-</span>
           </div>
-        : null}
+          : null}
 
         {rating ?
           <div className="bg-[#7575FE] flex items-center rounded-full p-1">
             <span className="px-2">{rating}</span>
             <RatingHeartIcon />
           </div>
-        : null}
+          : null}
       </div>
       <section className="overflow-visible pt-2">
         <div className="flex flex-col gap-2">
@@ -39,7 +57,6 @@ export default function LandingCard2({ title, location, bathroom, bedroom, parki
 
           <div className="flex items-center gap-1 text-right">
             <MapLocationIcon />
-
             <span className="font-medium">{location}</span>
           </div>
 
@@ -47,7 +64,6 @@ export default function LandingCard2({ title, location, bathroom, bedroom, parki
             {bathroom > 0 ?
               <div className="flex items-center">
                 <BathroomIcon />
-
                 <span className="mx-1 text-nowrap">{bathroom} حمام</span>
               </div>
               : null}
@@ -55,7 +71,6 @@ export default function LandingCard2({ title, location, bathroom, bedroom, parki
             {bedroom > 0 ?
               <div className="flex items-center">
                 <BedroomIcon />
-
                 <span className="mx-1 text-nowrap">{bedroom} خواب</span>
               </div>
               : null}
@@ -63,7 +78,6 @@ export default function LandingCard2({ title, location, bathroom, bedroom, parki
             {parking > 0 ?
               <div className="flex items-center">
                 <ParkingIcon />
-
                 <span className="mx-1 text-nowrap">{parking} پارکینگ</span>
               </div>
               : null}
@@ -71,8 +85,6 @@ export default function LandingCard2({ title, location, bathroom, bedroom, parki
             {yard ?
               <div className="flex items-center">
                 <YardIcon />
-
-
                 <span className="mx-1">حیاط دار</span>
               </div>
               : null}
@@ -82,7 +94,7 @@ export default function LandingCard2({ title, location, bathroom, bedroom, parki
             {originalPrice ?
               <>
                 <div className='relative flex gap-1 items-end text-[#A6A6A6]'>
-                  <span className="font-bold text-2xl ">{originalPrice}</span>
+                  <span className="font-bold text-2xl leading-6">{originalPrice}</span>
                   <TomanIcon fill='#A6A6A6' />
                   <div className="absolute top-1/2 left-0 w-full h-[1px] bg-red-500 rotate-[-10deg]"></div>
                 </div>
@@ -91,7 +103,7 @@ export default function LandingCard2({ title, location, bathroom, bedroom, parki
               </>
               : null}
 
-            <span className="font-bold text-2xl">{price}</span>
+            <span className="font-bold text-2xl leading-6">{price}</span>
             <TomanIcon />
           </div>
 
