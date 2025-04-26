d7c912afd8b3fd9ac4157491869bec038f5d4b99
Shayan <nwryshayan500@gmail.com>
2025-04-26 08:42:16 +0330

    basic implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import LandingCard1 from "../components/common/landing-card-1";
+import LandingCard2 from "../components/common/landing-card-2";
+
 export default function Home() {
   return (
-    <div>main page</div>
+    <>
+      <div className="grid grid-cols-4">
+        <LandingCard1 />
+        <LandingCard1 />
+        <LandingCard1 />
+        <LandingCard1 />
+      </div>
+
+      <div className="grid grid-cols-4">
+        <LandingCard2 />
+        <LandingCard2 />
+        <LandingCard2 />
+        <LandingCard2 />
+      </div>
+    </>
   );
 }
