960a6200e4b8b74f5898b730928ff59e10ed6456
qhoncheh <ghoncheh.ataee@gmail.com>
2025-04-28 18:36:50 +0330

    svg

--- src/assets/sgvs.tsx
+++ src/assets/sgvs.tsx
@@ -1,3 +1,4 @@
+
 export const MapLocationIcon = () => (
   <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect width="32" height="32" rx="16" fill="#F3F3F3" />
@@ -92,3 +93,31 @@ export const ClockTimerIcon = () => (
     <path d="M18.8475 4.17041C19.0217 4.3242 19.1911 4.48354 19.3555 4.648C19.5199 4.81246 19.6791 4.98203 19.8328 5.15629M15 2C15.4821 2.14255 15.9548 2.32634 16.4134 2.54664M21.4375 7.55457C21.6647 8.02313 21.8539 8.50663 22 9" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
 )
+
+
+
+
+export const CardSvg: React.FC = () => {
+  return (
+    <svg width="1" height="170" viewBox="0 0 1 199" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="0.5" y1="-2.18557e-08" x2="0.500009" y2="199" stroke="#D1D1D1" stroke-dasharray="5 5" />
+    </svg>
+
+  );
+};
+
+export const CardSvg2: React.FC = () => {
+  return (
+    <svg width="1" height="126" viewBox="0 0 1 126" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="0.5" y1="-2.18557e-08" x2="0.500005" y2="126" stroke="#BABAFF" stroke-dasharray="5 5" />
+    </svg>
+  );
+};
+
+export const SearchSvg: React.FC = () => {
+  return (
+    <svg width="64" height="64" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M13.0704 6.17297L21.1152 9.32092C25.7552 11.1366 28.0752 12.0444 27.9982 13.4844C27.9213 14.9245 25.5001 15.5848 20.6577 16.9055C19.216 17.2987 18.495 17.4953 17.9952 17.9951C17.4953 18.4949 17.2988 19.2159 16.9054 20.6577C15.5849 25.5 14.9245 27.9212 13.4845 27.9981C12.0444 28.0751 11.1366 25.7551 9.32098 21.1151L6.17303 13.0703C4.27213 8.21245 3.32167 5.78352 4.55262 4.55256C5.78358 3.32161 8.21253 4.27207 13.0704 6.17297Z" stroke="white" strokeWidth="1.5" strokeLinejoin="round" />
+    </svg>
+  );
+};
