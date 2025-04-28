be9b0535a1470271bff1dec11ec1de5f76747fba
Pouya <titanpooyanote@gmail.com>
2025-04-28 14:13:26 +0330

    custom color added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
             <Header />
           </header>
           <main>{children}</main>
-          <footer className="rounded-3xl bg-[#F9F9F9] dark:bg-stone-600 w-full px-7 py-8 flex flex-col gap-12">
+          <footer className="rounded-3xl bg-[#F9F9F9] dark:bg-darkMode w-full px-7 py-8 flex flex-col gap-12">
             <FooterContainer />
           </footer>
 
