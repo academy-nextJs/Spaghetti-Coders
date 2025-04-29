4dca8223f96e9c542f6186000101eaa209b6bbb8
qhoncheh <ghoncheh.ataee@gmail.com>
2025-04-29 11:52:52 +0330

    leaflet & map created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FilterPanel from "../components/common/Filter-house-reserve/FilterModal";
 // import LandingShinyAlphaSection from "../components/Landing/LandingShinyAlphaSection/LandingShinyAlphaSection";
+import 'leaflet/dist/leaflet.css';
+import LeafletMap from "../components/common/map/LeafletMap";
 
 export default function Home() {
   return (
@@ -7,6 +9,7 @@ export default function Home() {
       <div className="pt-10">
         {/* <LandingShinyAlphaSection /> */}
         <FilterPanel />
+        <LeafletMap /> 
       </div>
     </>
   );
