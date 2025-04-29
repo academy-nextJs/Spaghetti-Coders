25bde7089e7bde1997b3d029a2e14e336a440077
qhoncheh <ghoncheh.ataee@gmail.com>
2025-04-29 11:58:43 +0330

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import FilterPanel from "../components/common/Filter-house-reserve/FilterModal";
-// import LandingShinyAlphaSection from "../components/Landing/LandingShinyAlphaSection/LandingShinyAlphaSection";
-import 'leaflet/dist/leaflet.css';
-import LeafletMap from "../components/common/map/LeafletMap";
+import LandingShinyAlphaSection from "../components/Landing/LandingShinyAlphaSection/LandingShinyAlphaSection";
 
 export default function Home() {
   return (
     <>
       <div className="pt-10">
-        {/* <LandingShinyAlphaSection /> */}
-        <FilterPanel />
-        <LeafletMap /> 
+        <LandingShinyAlphaSection />
       </div>
     </>
   );
