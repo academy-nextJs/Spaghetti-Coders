a0783b80492e77bc146b178407635dbd9260d12a
qhoncheh <ghoncheh.ataee@gmail.com>
2025-05-06 11:56:08 +0330

    herosection's modal refactor

--- src/components/HomeContainer/HeroSection/ImageHero.tsx
+++ src/components/HomeContainer/HeroSection/ImageHero.tsx
@@ -1,9 +1,8 @@
-import SearchModal from "./SearchModal";
+import ModalSearch from "./SearchModal";
 import Image from "next/image";
-// import SearchButton from "./SearchButton"
 const ImageHero = () => {
   return (
-    <div className="w-full h-[35.35rem]  sm:flex-center rounded-2xl relative">
+    <div className="md:w-full md:h-[35.35rem] h-[12rem] md-[20rem] sm:flex-center rounded-2xl relative">
       <Image
         src="/hero.png"
         alt="Modern apartment building"
@@ -11,7 +10,7 @@ const ImageHero = () => {
         className="rounded-3xl relative  w-full object-cover"
       />
       <div className="lg:absolute  lg:top-1/2 lg:left-2.5 transform -translate-x-1/2 -translate-y-1/2  ">
-        <SearchModal />
+        <ModalSearch />
       </div>
     </div>
   );
--- src/components/HomeContainer/HeroSection/SearchModal.tsx
+++ src/components/HomeContainer/HeroSection/SearchModal.tsx
@@ -1,108 +1,25 @@
-// import { FilterModalProps } from '@/src/types/types';
-// import ReactDOM from 'react-dom';
-// import Calender from '../../common/Calender/calender';
-// import { ClientButton } from '../../common/Buttons/common-btn';
-// import SelectClient from '../../common/SelectClient';
-// import { ClientInput } from '../../common/inputs/clientInput';
+"use client"
 
-// const SearchModal: React.FC<FilterModalProps> = ({ isOpen, onClose }) => {
-//   if (!isOpen) return null;
-
-//   const handleBackgroundClick = (e: React.MouseEvent) => {
-//     if (e.target === e.currentTarget) {
-//       onClose();
-//     }
-//   };
-
-//   const cities = [
-//     { key: 'babol', label: 'بابل' },
-//     { key: 'sari', label: 'ساری' },
-//   ];
-
-//   return ReactDOM.createPortal(
-//     <div
-//       className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/40 backdrop-gray-500"
-//       onClick={handleBackgroundClick}
-//     >
-//       <div className="w-[700px] rounded-xl bg-[#E9E9E9] dark:bg-darkMode p-6 shadow-xl relative">
-//         <div className="flex flex-row gap-2 mb-6">
-//           <ClientButton className='h-9 bg-[#7575FE] text-white '>
-//             <div className='rounded-full bg-white w-2 h-2'></div>
-//             رهن و اجاره
-//           </ClientButton>
-//           <ClientButton className='h-9 bg-[#F0F0F0] text-black dark:bg-gray-800 dark:text-white'>
-//             رزرو ملک
-//           </ClientButton>
-//           <ClientButton className='h-9 bg-[#F0F0F0] text-black dark:bg-gray-800 dark:text-white'>
-//             خرید و فروش
-//           </ClientButton>
-//         </div>
-//         <aside className='flex flex-row gap-6'>
-//           <aside className='flex flex-col w-1/2'>
-//             <span className="text-sm font-medium text-right mb-2">انتخاب مقصد</span>
-//             <SelectClient
-//               items={cities}
-//               className="w-full"
-//               placeholder="انتخاب کنید"
-//             />
-//           </aside>
-
-//           <aside className='flex flex-col w-1/2'>
-//           <span className="text-sm font-medium text-right mb-2">نفرات تعداد </span>
-//             <ClientInput              
-//               placeholder="وارد کنید"
-//             />
-//           </aside>
-//         </aside>
-
-//         <aside className='flex flex-row mt-4 font-medium gap-6'>
-//           <aside className='flex flex-col w-1/2'>
-//             <span className="text-sm font-medium text-right mb-2">تاریخ ورود</span>
-//             <Calender />
-//           </aside>
-
-//           <aside className='flex flex-col w-1/2'>
-//             <span className="text-sm font-medium text-right mb-2">تاریخ خروج</span>
-//             <Calender />
-//           </aside>
-//         </aside>
-
-//         <aside className="mt-6 flex">
-//           <ClientButton className='h-9 bg-[#7575FE] text-white'>
-//             مشاهده نتیجه
-//           </ClientButton>
-//         </aside>
-//       </div>
-//     </div>,
-//     document.getElementById('modal-root') as HTMLElement
-//   );
-// };
-
-// export default SearchModal;
-
-"use client";
-
-import React, { useRef } from "react";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  useDisclosure,
-  useDraggable,
-} from "@heroui/react";
+import React from "react";
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@heroui/react";
 import { ClientButton } from "../../common/Buttons/common-btn";
-import SelectClient from "../../common/SelectClient";
 import { ClientInput } from "../../common/inputs/clientInput";
+import SelectClient from "../../common/SelectClient";
+import Calender from "../../common/Calender/calender";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Cursor02Icon } from "@hugeicons/core-free-icons";
 import { City } from "@/src/types/types";
 
+export default function ModalSearch() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [size, setSize] = React.useState("md");
 
-export default function SearchModal() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const targetRef = useRef<HTMLDivElement>(null);
-  const { moveProps } = useDraggable({ canOverflow: true, isDisabled: !isOpen });
+  const sizes = ["3xl"];
+
+  const handleOpen = (size: string) => {
+    setSize(size);
+    onOpen();
+  };
 
   const cities: City[] = [
     { key: "babol", label: "بابل" },
@@ -111,52 +28,67 @@ export default function SearchModal() {
 
   return (
     <>
-      <Button
-        onPress={onOpen}
-        className="w-28 h-28 rounded-full text-md border-4 text-white border-white hover:bg-[#7575FE] bg-[#7575FE]"
-      >
-        جستجو
-        <br />
-        سریع
-      </Button>
+      <div className="flex flex-wrap gap-3">
+        {sizes.map((size) => (
+          <Button
+            key={size}
+            onPress={() => handleOpen(size)}
+            className="w-32 h-32 rounded-full text-md border-4 text-white border-white hover:bg-[#7575FE] bg-[#7575FE]"
+          >
+            <div className="flex flex-col text-sm justify-center">
+              <HugeiconsIcon icon={Cursor02Icon} size={45} className="pr-2" />
+              جستجوی
+              <br />
+              سریع
+            </div>
+          </Button>
+        ))}
+      </div>
 
-      <Modal  ref={targetRef} isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent className="w-full">
-          <>
-            <style jsx global>{`
-              button[aria-label="Close"] {
-                display: none !important;
-              }
-            `}</style>
+      <Modal isOpen={isOpen} size={size} onClose={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-row gap-2">
+                <ClientButton className="h-9 bg-[#7575FE] text-white">رزرو ملک</ClientButton>
+                <ClientButton className="h-9 bg-[#F0F0F0] text-black dark:bg-darkMode dark:text-white">رهن و اجاره</ClientButton>
+                <ClientButton className="h-9 bg-[#F0F0F0] text-black dark:bg-darkMode dark:text-white">خرید و فروش</ClientButton>
+              </ModalHeader>
 
-            <ModalHeader {...moveProps} className="flex flex-row gap-1">
-              <ClientButton className="h-9 bg-[#7575FE] text-white">
-                رزرو ملک
-              </ClientButton>
-              <ClientButton className="h-9 bg-[#F0F0F0] text-black dark:bg-gray-800 dark:text-white">
-                رهن و اجاره
-              </ClientButton>
-              <ClientButton className="h-9 bg-[#F0F0F0] text-black dark:bg-gray-800 dark:text-white">
-                خرید و فروش
-              </ClientButton>
-            </ModalHeader>
+              <ModalBody className="flex flex-col gap-4">
+                <div className="flex flex-row gap-5 mr-8">
+                  <div className="flex flex-col w-[20rem]">
+                    <span className="text-sm font-medium text-right pr-2">انتخاب مقصد</span>
+                    <SelectClient items={cities} placeholder="انتخاب کنید" />
+                  </div>
+                  <div className="flex flex-col w-[20rem]">
+                    <span className="text-sm font-medium text-right">تعداد نفرات</span>
+                    <ClientInput placeholder="انتخاب کنید" />
+                  </div>
+                </div>
 
-            <ModalBody className="flex flex-row gap-4">
-              <div className="flex flex-col w-1/2">
-                <span className="text-sm font-medium text-right pr-2">انتخاب مقصد</span>
-                <SelectClient items={cities} placeholder="انتخاب کنید" />
-              </div>
-              <div className="flex flex-col w-1/2">
-                <span className="text-sm font-medium text-right">تعداد نفرات</span>
-                <ClientInput placeholder="انتخاب کنید" />
-              </div>
-            </ModalBody>
+                <div className="flex flex-row mt-4 mr-8 font-medium">
+                  <div className="flex flex-col w-1/2">
+                    <span className="text-sm font-medium text-right mb-2">تاریخ ورود</span>
+                    <Calender />
+                  </div>
+                  <div className="flex flex-col w-1/2">
+                    <span className="text-sm font-medium text-right mb-2">تاریخ خروج</span>
+                    <Calender />
+                  </div>
+                </div>
+              </ModalBody>
 
-            <ModalFooter className="flex flex-row justify-start gap-2">
-              <Button className="bg-[#F0F0F0] text-black rounded-full">بستن</Button>
-              <Button className="bg-[#7575FE] rounded-full text-white">مشاهده نتیجه</Button>
-            </ModalFooter>
-          </>
+              <ModalFooter className="flex flex-row justify-start mt-5">
+                <Button onPress={onClose} className="rounded-full bg-[#F0F0F0] dark:bg-darkMode">
+                  بستن
+                </Button>
+                <Button onPress={onClose} className="rounded-full bg-[#7575FE] text-white w-32">
+                  مشاهده نتیجه
+                </Button>
+              </ModalFooter>
+            </>
+          )}
         </ModalContent>
       </Modal>
     </>
