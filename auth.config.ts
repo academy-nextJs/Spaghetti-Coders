39a6a941c6f61c3b2e3fea5ac4a0631be9a4bd70
Shayan <nwryshayan500@gmail.com>
2025-05-16 14:36:24 +0330

    Bugfix: Login error handling

--- auth.config.ts
+++ auth.config.ts
@@ -6,40 +6,34 @@ import Google from "next-auth/providers/google";
 const BASE_URL = process.env.BASE_URL
 
 class InvalidLoginError extends CredentialsSignin {
-  code = "Invalid Email or Password"
+  message = "ایمیل یا کلمه عبور شما صحیح نیست!"
 }
 
-export default { 
+export default {
   providers: [
     Google,
     GitHub,
     Credentials({
       authorize: async (credentials) => {
-        // try {
-          let user = null
+        let user = null
 
-          const res = await fetch(`${BASE_URL}/auth/login`, {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              email: credentials?.email,
-              password: credentials?.password,
-            }),
-          });
+        const res = await fetch(`${BASE_URL}/auth/login`, {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password,
+          }),
+        });
 
-          user = await res.json();
-          console.log('credentials', credentials)
-          console.log('user', user)
-          if(!res.ok || !user) {
-            throw new InvalidLoginError()
-            // return null
-          }
-          // if(!res.ok || !user) return null
-          
-          return user;
-        // } catch {
-        //   return null;
-        // }
+        user = await res.json();
+        console.log('credentials', credentials)
+        console.log('user', user)
+        if(!res.ok || !user) {
+          throw new InvalidLoginError()
+          // return null
+        }          
+        return user;
       },
     })
   ],
