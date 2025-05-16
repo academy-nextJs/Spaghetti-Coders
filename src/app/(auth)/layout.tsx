ae022018a54be78703b397ba57b22f2d7253b78f
Shayan <nwryshayan500@gmail.com>
2025-05-16 21:44:33 +0330

    Feature: ALFA logo link

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Logo from '@/public/AlFA.svg';
 import SwiperSlider from "@/src/components/common/Auth/SwiperSlider";
+import Link from "next/link";
 
 export default function AuthLayout({
   children,
@@ -18,22 +19,24 @@ export default function AuthLayout({
           {children}
         </main>
 
-        <div className='hidden md:block absolute left-1/2 transform -translate-x-1/2 top-15 z-10 dark:invert-100'>
-          <Image
-            className="relative z-10"
-            src={Logo.src}
-            alt="Logo"
-            width={78}
-            height={78}
-          />
-          <Image
-            className="absolute left-0 top-0 h-full w-1/2 object-left object-cover invert z-20"
-            src={Logo.src}
-            alt="Logo"
-            width={78}
-            height={78}
-          />
-        </div>
+        <Link href='/'>
+          <div className='hidden md:block absolute left-1/2 transform -translate-x-1/2 top-15 z-10 dark:invert-100'>
+            <Image
+              className="relative z-10"
+              src={Logo.src}
+              alt="Logo"
+              width={78}
+              height={78}
+            />
+            <Image
+              className="absolute left-0 top-0 h-full w-1/2 object-left object-cover invert z-20"
+              src={Logo.src}
+              alt="Logo"
+              width={78}
+              height={78}
+              />
+          </div>
+        </Link>
 
         <SwiperSlider />
         {/* <SvgBlue /> */}
