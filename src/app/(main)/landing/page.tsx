789b0c183efe64d0b2156bbd7c8ea74843f8bffa
Pouya <titanpooyanote@gmail.com>
2025-05-16 13:45:42 +0330

    category shine added

--- src/app/(main)/landing/page.tsx
+++ src/app/(main)/landing/page.tsx
@@ -2,14 +2,37 @@ import React from 'react';
 import api from '@/src/services/api';
 import dynamic from 'next/dynamic';
 
-const DreamSection = dynamic(() => import('@/src/components/Landing/dreamDestination/dreamSectionContainer'));
-const CategoryCarousel = dynamic(() => import('@/src/components/Landing/landingCategory/categoryCarousel'));
-const HeroSection = dynamic(() => import('@/src/components/Landing/HeroSection/index'));
-const LandingShinyAlphaSection = dynamic(() => import('@/src/components/Landing/LandingShinyAlphaSection/LandingShinyAlphaSection'));
-const LandingComments = dynamic(() => import('@/src/components/Landing/comments/commentsContainer'));
-const LandingDiscountedSection = dynamic(() => import('@/src/components/Landing/LandingDiscountedSection/LandingDiscountedSection'));
-const LandingBuySellSection = dynamic(() => import('@/src/components/Landing/LandingBuySellSection/LandingBuySellSection'));
-
+const DreamSection = dynamic(
+  () =>
+    import('@/src/components/Landing/dreamDestination/dreamSectionContainer')
+);
+const CategoryCarousel = dynamic(
+  () => import('@/src/components/Landing/landingCategory/categoryCarousel')
+);
+const HeroSection = dynamic(
+  () => import('@/src/components/Landing/HeroSection/index')
+);
+const LandingShinyAlphaSection = dynamic(
+  () =>
+    import(
+      '@/src/components/Landing/LandingShinyAlphaSection/LandingShinyAlphaSection'
+    )
+);
+const LandingComments = dynamic(
+  () => import('@/src/components/Landing/comments/commentsContainer')
+);
+const LandingDiscountedSection = dynamic(
+  () =>
+    import(
+      '@/src/components/Landing/LandingDiscountedSection/LandingDiscountedSection'
+    )
+);
+const LandingBuySellSection = dynamic(
+  () =>
+    import(
+      '@/src/components/Landing/LandingBuySellSection/LandingBuySellSection'
+    )
+);
 
 export default async function LandingPage() {
   const { data } = await api.get('/categories');
@@ -17,24 +40,15 @@ export default async function LandingPage() {
   return (
     <div className="w-full flex flex-col gap-20 justify-center">
       <HeroSection />
-      <div className="flex justify-end">
+      <div className="flex items-center w-full justify-between">
+        <div className="circle scale-125 -mr-36" />
         <CategoryCarousel data={data} />
       </div>
-      <div className="">
-        <LandingDiscountedSection />
-      </div>
-      <div className="">
-        <DreamSection />
-      </div>
-      <div className=" ">
-        <LandingShinyAlphaSection />
-      </div>
-      <div className="">
-        <LandingBuySellSection />
-      </div>
-      <div className="">
-        <LandingComments />
-      </div>
+      <LandingDiscountedSection />
+      <DreamSection />
+      <LandingShinyAlphaSection />
+      <LandingBuySellSection />
+      <LandingComments />
     </div>
   );
 }
