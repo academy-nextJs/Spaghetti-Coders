bcac0d4ba317abe9c43ccd3ce790684420d9fb0a
Shayan <nwryshayan500@gmail.com>
2025-05-17 22:45:40 +0330

    Feature: reserve page almost done

--- src/components/reservePage/HouseReserveCardsGrid.tsx
+++ src/components/reservePage/HouseReserveCardsGrid.tsx
@@ -1,8 +1,9 @@
+import { Ref } from "react";
 import LandingHouseCard from "../Landing/LandingHouseCard/LandingHouseCard";
 
-export default function HouseReserveCardsGrid() {
+export default function HouseReserveCardsGrid({ ref }: { ref: Ref<HTMLElement> | undefined}) {
   return (
-    <section className=" grid grid-cols-2 lg:grid-cols-3 gap-8">
+    <section ref={ref} className="w-fit grid grid-cols-2 gap-8">
       {Array(10).fill(null).map((_, index) =>
         <LandingHouseCard
           key={index}
