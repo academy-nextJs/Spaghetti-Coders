cbbfb96571816779d7e455d51f05fdcafb8938f1
Shayan <nwryshayan500@gmail.com>
2025-05-24 17:49:38 +0330

    Improvement: Separation of Reserve Container's logic

--- src/components/reservePage/reserveContainer.tsx
+++ src/components/reservePage/reserveContainer.tsx
@@ -12,8 +12,9 @@ import {
 } from '@heroui/react';
 import ReserveFilterDrawer from './reserveFilterDrawer';
 import { ReserveContainerProps } from '@/src/types/types';
-import { useEffect, useMemo, useRef } from 'react';
-import { throttle } from 'lodash';
+import { useEffect } from 'react';
+import { useReserveHelpers } from '@/src/hooks/useReserveHelpers';
+
 const HouseReserveCardsGrid = dynamic(() => import('./HouseReserveCardsGrid'), {
   ssr: false,
 });
@@ -25,72 +26,17 @@ export default function ReserveContainer({ locations }: ReserveContainerProps) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const updateFilter = useUpdateFilter();
 
-  const mapWidth = useRef<number>(60);
-  const isResizing = useRef(false);
+  const {
+    mapRef,
+    cardRef,
+    gridRef,
+    startResizing,
+    stopResizing,
+    throttledHandleResizing,
+    throttledSetGridColumns,
+  } = useReserveHelpers()
 
-  const mapRef = useRef<HTMLDivElement>(null);
-  const cardRef = useRef<HTMLDivElement>(null);
-  const gridRef = useRef<HTMLElement>(null); //Section Element
-
-  function calculateGridColumns(mapWidth: number): string {
-    const tablet = window.innerWidth < 1250;
-    const breakpoints = [
-      { width: 40, cols: 4 },
-      { width: 55, cols: 3 },
-      { width: 70, cols: 2 },
-      { width: 80, cols: 1 },
-    ];
-
-    const columns =
-      breakpoints.find((point) => mapWidth < point.width)?.cols || 4;
-    return `repeat(${tablet ? columns - 1 : columns}, minmax(0, 1fr))`;
-  }
-
-  function setGridColumns() {
-    //this logic must be separate as a function, cause we should pass it to our resize eventListener
-    if (gridRef.current)
-      gridRef.current.style.gridTemplateColumns = calculateGridColumns(
-        mapWidth.current
-      );
-  }
-
-  const startResizing = () => {
-    isResizing.current = true;
-    document.body.style.userSelect = 'none';
-  };
-
-  const handleResizing = (e: MouseEvent) => {
-    if (!isResizing.current) return;
-
-    const calculatedMapWidth = (e.clientX / window.innerWidth) * 100;
-
-    if (window.innerWidth >= 1024) {
-      if (calculatedMapWidth >= 20 && calculatedMapWidth < 80) {
-        if (mapRef.current)
-          mapRef.current.style.width = calculatedMapWidth + '%';
-        if (cardRef.current)
-          cardRef.current.style.width = 100 - calculatedMapWidth + '%';
-        mapWidth.current = calculatedMapWidth; //set the map's width in a ref to be accessible outside of this function
-
-        setGridColumns(); //wrapped this logic inside a function
-      }
-    }
-  };
-
-  const stopResizing = () => {
-    isResizing.current = false;
-    document.body.style.userSelect = '';
-  };
-
-  const throttledHandleResizing = useMemo(
-    () => throttle(handleResizing, 100),
-    []
-  );
-  const throttledSetGridColumns = useMemo(
-    () => throttle(setGridColumns, 500),
-    []
-  );
-
+  
   useEffect(() => {
     window.addEventListener('mousemove', throttledHandleResizing);
     window.addEventListener('mouseup', stopResizing);
