24737077810bf1529cb64bb43169cac4e40fadd2
qhoncheh <ghoncheh.ataee@gmail.com>
2025-05-25 10:19:23 +0330

    Hero modal

--- src/components/Landing/HeroSection/SearchModal.tsx
+++ src/components/Landing/HeroSection/SearchModal.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   Button,
   Modal,
@@ -15,21 +15,39 @@ import SelectClient from '../../common/SelectClient';
 import { HugeiconsIcon } from '@hugeicons/react';
 import { Cursor02Icon } from '@hugeicons/core-free-icons';
 import { City } from '@/src/types/types';
+import { useSearchParams } from 'next/navigation';
 
 export default function ModalSearch() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [size, setSize] = React.useState<
-    'md' | '3xl' | 'xs' | 'sm' | 'lg' | 'xl' | '2xl' | '4xl' | '5xl' | 'full'>('md');
+    'md' | '3xl' | 'xs' | 'sm' | 'lg' | 'xl' | '2xl' | '4xl' | '5xl' | 'full'
+  >('md');
   const sizes = ['3xl'] as const;
   const handleOpen = (size: string) => {
-    setSize(
-      size as | 'md' | '3xl' | 'xs' | 'sm' | 'lg' | 'xl' | '2xl' | '4xl' | '5xl' | 'full');
+    setSize(size as 'md' | '3xl' | 'xs' | 'sm' | 'lg' | 'xl' | '2xl' | '4xl' | '5xl' | 'full');
     onOpen();
   };
+  
+  const searchParams = useSearchParams();
+  const queryValues = useMemo(() => {
+    const params = new URLSearchParams(searchParams);
+    return {
+      location: params.get('location') || '',
+      category: params.get('category') || '',
+      order: params.get('order') || '',
+      propertyType: params.get('propertyType') || '',
+      transactionType: params.get('transactionType') || '',
+      maxPrice: params.get('maxPrice') || '',
+      minRent: params.get('minRent') || '',
+    };
+  }, [searchParams]);
+
+
   const cities: City[] = [
     { key: 'babol', label: 'بابل' },
     { key: 'sari', label: 'ساری' },
   ];
+
   return (
     <>
       <div className="flex flex-wrap gap-3">
@@ -62,41 +80,47 @@ export default function ModalSearch() {
                     <span className="text-sm font-medium text-right pr-2">
                       محل مورد نظر
                     </span>
-                    <SelectClient items={cities} placeholder="انتخاب کنید" />
+                    <SelectClient
+                      items={cities}
+                      placeholder="انتخاب کنید"
+                      value={queryValues.location} 
+                    />
                   </div>
                   <div className="flex flex-col w-[20rem]">
                     <span className="text-sm font-medium text-right">
                       نوع ملک
                     </span>
-                   <SelectClient items={cities} placeholder="انتخاب کنید" />
+                    <SelectClient
+                      items={cities}
+                      placeholder="انتخاب کنید"
+                      value={queryValues.propertyType} 
+                    />
                   </div>
                 </div>
                 <div className="flex flex-row mt-4 mr-8 font-medium">
                   <div className="flex flex-col w-1/2">
                     <span className="text-sm font-medium text-right mb-2">
-                       حداقل اجاره 
+                      حداقل اجاره
                     </span>
-                    <ClientInput type="number" />
+                    <ClientInput
+                      value={queryValues.minRent} 
+                    />
                   </div>
                   <div className="flex flex-col w-1/2">
                     <span className="text-sm font-medium text-right mb-2">
-                       حداکثر اجاره
+                      حداکثر اجاره
                     </span>
-                    <ClientInput type="number" />
+                    <ClientInput
+                      value={queryValues.maxPrice} 
+                    />
                   </div>
                 </div>
               </ModalBody>
-
               <ModalFooter className="flex flex-row justify-start mt-5">
-                <Button
-                  onPress={onClose}
-                  className="rounded-full bg-[#F0F0F0] dark:bg-darkMode"
-                >
+                <Button onPress={onClose} className="rounded-full bg-[#F0F0F0] dark:bg-darkMode">
                   بستن
                 </Button>
-                <Button
-                  className="rounded-full bg-[#7575FE] text-white w-32"
-                >
+                <Button className="rounded-full bg-[#7575FE] text-white w-32">
                   مشاهده نتیجه
                 </Button>
               </ModalFooter>
