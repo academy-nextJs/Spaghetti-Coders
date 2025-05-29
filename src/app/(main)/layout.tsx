ef947e672e579b0ecff9f09f67d657b706c8251b
Shayan <nwryshayan500@gmail.com>
2025-05-29 18:09:52 +0330

    Bugfix: Fix for Transition Layout shift

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="w-[88%] md:w-[95%] lg:w-[97%] m-auto py-4">
+    <main className="w-[88%] md:w-[95%] lg:w-[97%] m-auto">
       <Header />
       <main className='mt-20'>{children}</main>
       <footer className="rounded-3xl bg-[#F9F9F9] dark:bg-darkMode m-auto px-7 py-8 flex flex-col gap-12 mt-12">
