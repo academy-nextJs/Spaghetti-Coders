420fb650c0ffbd1eecec3725ff076bf6f026e545
Shayan <nwryshayan500@gmail.com>
2025-06-01 18:47:35 +0330

    Feature: ALFA Logo added

--- src/components/PageTransition/Revealer.tsx
+++ src/components/PageTransition/Revealer.tsx
@@ -1,14 +1,17 @@
 'use client'
 
-import { motion } from "framer-motion";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import Logo from '@/public/AlFA.svg';
+
 
 export function Revealer() {
   const pathname = usePathname()
   return (
     <motion.div 
       key='revealer'
-      className="fixed top-0 left-0 w-screen h-screen origin-[top_center] bg-primaryPurple pointer-events-none z-[100] flex justify-center items-center"
+      className="fixed top-0 left-0 w-screen h-screen origin-[top_center] bg-primaryPurple pointer-events-none z-[100] flex flex-row-reverse justify-center items-center"
       initial={{ scaleY: 1 }}
       animate={{ scaleY: 0 }}
       transition={{
@@ -17,8 +20,16 @@ export function Revealer() {
         ease: [0.9, 0, 0.1, 1]
       }}
     >
-      <span className="font-mono font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-l from-primaryPurple via-white to-white">
+      <Image
+        className="justify-center dark:invert-100 size-[12rem]"
+        src={Logo.src}
+        alt="Logo"
+        width={78}
+        height={78}
+      />
+      <span className="font-mono font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-l from-transparent via-white to-white">
         {pathname.slice(1)}
+        {"/"}
       </span>
     </motion.div>
   );
